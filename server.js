410d6b407c9008289f70d045c5ca086f71f82fd6
ashpokk <ayeshaeman112@gmail.com>
2026-07-25 17:42:00 +0500

    Stage 3: Post new task

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.use(express.json())
 const tasks  =  [
     {
         id: 1,
@@ -46,6 +47,23 @@ app.get('/tasks', (req, res) => {
      )
  })
 
+
+ app.post('/tasks', (req, res) =>{
+         const {title, done} = req.body
+         if (!title || typeof done !== 'boolean' || title.trim() === '') {
+            return res.status(400).json({error : "Needs a title and done status"})
+            }
+            const newTask = {
+    id: tasks.length+1,
+    title: title,
+    done: done
+}
+tasks.push(newTask)
+res.status(201).json(newTask)
+
+        }
+            )
+    
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`)
 })
