d140a543f6173451358aaf0e16e8c83b95a1029e
ashpokk <ayeshaeman112@gmail.com>
2026-08-28 14:13:04 +0500

    Stage 2: public route and unverified protected route

--- server.js
+++ server.js
@@ -28,6 +28,36 @@ app.get('/', (req, res) => {
 })
 
 
+app.get('/public/info', (req, res) => {
+    res.status(200).json({
+        message: "Welcome stranger! This info is public."
+    });
+});
+
+
+app.get('/protected/profile', (req, res) => {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {
+        return res.status(401).json({
+            error: "Access token required"
+        });
+    }
+
+    const token = authHeader.split(' ')[1];
+
+    if (!token) {
+        return res.status(401).json({
+            error: "Access token required"
+        });
+    }
+
+    res.status(200).json({
+        message: "Protected profile"
+    });
+});
+
+
 /**
  * @swagger
  * /tasks:
